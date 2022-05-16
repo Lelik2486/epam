cb6f01c19b8bb7ff9999cb1d6ca094edf511e014
Lelik2486 <alexejzukov86@gmail.com>
2022-05-16 13:54:23 +0300

    add task1

---
+++ src/main/java/com/lelik/basicsOfSoftwareCodeDevelopment/Task1.java
@@ -0,0 +1,12 @@
+package com.lelik.basicsOfSoftwareCodeDevelopment;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double a = 3;
+        double b= 4;
+        double c = 6;
+
+        double z = ((a-3)*b/2) + c;
+        System.out.println(z);
+    }
+}
