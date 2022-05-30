6e88b9ba2a46167eacb9140dfe2fd83cd4c60c4f
Lelik2486 <alexejzukov86@gmail.com>
2022-05-30 10:38:35 +0300

    add task 7,task 8

--- src/main/java/com/lelik/algorithmization/oneDimensionalArrays/Task7.java
+++ src/main/java/com/lelik/algorithmization/oneDimensionalArrays/Task7.java
@@ -2,5 +2,23 @@ package com.lelik.algorithmization.oneDimensionalArrays;
 /*Даны действительные числа a1,a2,,an . Найти
 max( a1 + a2n,a2 + a2n−1,,an + an+1) .*/
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task7 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input array lenght");
+        int n = Integer.parseInt(reader.readLine());
+        double[] arr = new double[n];
+        InAndOutArray.fillingDoubleWithConsole(arr);
+        InAndOutArray.outArray(arr);
+        System.out.println();
+        double sumMax = arr[0] + arr[arr.length-1];
+        for (int i = 1; i < (arr.length-2)/2; i++) {
+            if (sumMax < arr[i]+arr[arr.length-i])sumMax = arr[arr.length-i];
+        }
+        System.out.println(sumMax);
+    }
 }
--- src/main/java/com/lelik/algorithmization/oneDimensionalArrays/Task8.java
+++ src/main/java/com/lelik/algorithmization/oneDimensionalArrays/Task8.java
@@ -2,5 +2,33 @@ package com.lelik.algorithmization.oneDimensionalArrays;
 /*Дана последовательность целых чисел a1,a2,,an . Образовать новую последовательность, выбросив из
 исходной те члены, которые равны min(a1,a2,,an)*/
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Task8 {
+    public static void main(String[] args)throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input array lenght");
+        int n = Integer.parseInt(reader.readLine());
+        int[] arr = new int[n];
+        InAndOutArray.fillingIntWithConsole(arr);
+        int min = arr[0];
+        int count = 0;
+        for (int i : arr) {
+            if (min > i){
+                min= i;
+                count++;
+            }
+        }
+        int[] newArr = new int[arr.length-count];
+        int indexNewArr =0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]!=min){
+                newArr[indexNewArr] = arr[i];
+                indexNewArr++;
+            }
+        }
+        InAndOutArray.outArray(newArr);
+    }
 }
