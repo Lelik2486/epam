81acda042337fc0fb76884bcf32f1be9615ad08e
Lelik2486 <alexejzukov86@gmail.com>
2022-06-07 18:42:16 +0300

    add matrix task 7

--- src/main/java/com/lelik/algorithmization/matrix/Task7.java
+++ src/main/java/com/lelik/algorithmization/matrix/Task7.java
@@ -1,23 +1,18 @@
 package com.lelik.algorithmization.matrix;
 
-/*Сформировать квадратную матрицу порядка n по заданному образцу(n - четное)*/
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Task6 {
+public class Task7 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Input number rows");
         int n = Integer.parseInt(reader.readLine());
         int[][] matrix = new int[n][n];
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n-i; j ++ ) {
-                matrix[i][j] = 1;
-            }
-            for (int j = n-i-1; j < i+1; j ++ ) {
-                matrix[i][j] = 1;
+            for (int j = 0; j < n; j++) {
+                matrix[i][j]= (int) Math.sin((Math.pow(i,2)-Math.pow(i,2))/n);
             }
         }
         InAndOut.outMatrix(matrix);
