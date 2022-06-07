8272b719bae403db5615f4e4af844f34e90420b2
Lelik2486 <alexejzukov86@gmail.com>
2022-06-07 18:48:51 +0300

    add matrix task 7 fix

--- src/main/java/com/lelik/algorithmization/matrix/Task7.java
+++ src/main/java/com/lelik/algorithmization/matrix/Task7.java
@@ -10,11 +10,14 @@ public class Task7 {
         System.out.println("Input number rows");
         int n = Integer.parseInt(reader.readLine());
         int[][] matrix = new int[n][n];
+        int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 matrix[i][j]= (int) Math.sin((Math.pow(i,2)-Math.pow(i,2))/n);
+                if (matrix[i][j]>0)count++;
             }
         }
         InAndOut.outMatrix(matrix);
+        System.out.println(count);
     }
 }
