1828caf3fd41480b04dfbd70c36b47f846166be0
Lelik2486 <alexejzukov86@gmail.com>
2022-10-04 11:14:30 +0300

    fix task 12

--- src/main/java/com/lelik/algorithmization/matrix/Task12.java
+++ src/main/java/com/lelik/algorithmization/matrix/Task12.java
@@ -3,6 +3,7 @@ package com.lelik.algorithmization.matrix;
 /*Отсортировать строки матрицы по возрастанию и убыванию значений элементов.*/
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Task12 {
     public static void main(String[] args) {
@@ -13,9 +14,34 @@ public class Task12 {
         int[][] matrix = InAndOut.newMatrix(m, n, min, max);
         InAndOut.outMatrix(matrix);
         System.out.println();
-        for (int[] ints : matrix) {
-            Arrays.sort(ints);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp;
+                for (int k = 0; k < n-1; k++) {
+                    if (matrix[i][k]>matrix[i][k+1]){
+                        tmp=matrix[i][k+1];
+                        matrix[i][k+1]=matrix[i][k];
+                        matrix[i][k]=tmp;
+                    }
+                }
+            }
         }
         InAndOut.outMatrix(matrix);
+        System.out.println();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int tmp;
+                for (int k = 0; k < n-1; k++) {
+                    if (matrix[i][k]<matrix[i][k+1]){
+                        tmp=matrix[i][k+1];
+                        matrix[i][k+1]=matrix[i][k];
+                        matrix[i][k]=tmp;
+                    }
+                }
+            }
+        }
+        InAndOut.outMatrix(matrix);
+        System.out.println();
+
     }
 }
