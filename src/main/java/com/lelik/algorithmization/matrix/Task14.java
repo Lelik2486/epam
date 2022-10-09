7c011f9bf9e104837a59b12509a3441a521ca0d5
Lelik2486 <alexejzukov86@gmail.com>
2022-10-09 18:39:24 +0300

    fix task 14

--- src/main/java/com/lelik/algorithmization/matrix/Task14.java
+++ src/main/java/com/lelik/algorithmization/matrix/Task14.java
@@ -1,8 +1,4 @@
 package com.lelik.algorithmization.matrix;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.Random;
 
 public class Task14 {
@@ -12,18 +8,14 @@ public class Task14 {
         Random random = new Random();
         int[][] matrix = new int[m][n];
         /* т.к. первый столбец имеет номер 0 то его можно заполнить нулями*/
-        for (int i = 0; i < m; i++) {
-            matrix[0][i]=0;
-        }
-        for (int i = 1; i < n; i++) {   /* проходим по столбцам*/
+        for (int i = 0; i < n; i++) {   /* проходим по столбцам*/
             int amount=0;   /* это счетчик количества единиц в столбце*/
-            if (matrix[0][i]==1)amount++;
             for (int j = 0; j < m; j++) {   /*поход внутри столбца  */
-                if(amount<i&&i==amount+m-j)matrix[j][i]=1;   /* если единиц меньше
+                if(amount<(i+1)&&(i+1)==amount+m-j)matrix[j][i]=1;   /* если единиц меньше
                 номера столбца и свободного места для нулей нет, то заполняем единицей */
-                else if(amount<i&&i<amount+m-j)matrix[j][i]= random.nextInt(0,2); /*
+                else if(amount<i+1&&(i+1)<amount+m-j)matrix[j][i]= random.nextInt(0,2); /*
                 если единиц меньше номера столбца и свободное место для нулей есть, то заполняем рандом */
-                else if(amount==i)matrix[j][i]=0;/*   если единиц достаточно, то заполняем 0  */
+                else if(amount==(i+1))matrix[j][i]=0;/*   если единиц достаточно, то заполняем 0  */
                 if (matrix[j][i]==1)amount++;
             }
         }
